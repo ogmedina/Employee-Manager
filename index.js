f21f49f5d4f6a715b0a3a509807da1b9a92148c5
Oscar Medina <ogmedina04@gmail.com>
2021-01-13 14:48:25 -0600

    added starting files with database seed, early user prompt data, and package dependencies

---
+++ index.js
@@ -0,0 +1,84 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const CFonts = require("cfonts");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_trackerdb"
+})
+
+connection.connect(function(err){
+    if (err) throw err;
+    CFonts.say('Employee|Manager', {
+    font: '3d',              
+	align: 'left',           
+	colors: ['red', "cyan"], 
+	background: 'transparent',
+	letterSpacing: 1,         
+	lineHeight: 1,            
+	space: true,              
+	maxLength: '0',           
+	gradient: false,           
+	independentGradient: false, 
+	transitionGradient: false,  
+	env: 'node'     
+    });    
+    promptUser();
+});
+
+function promptUser() {
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "Done!"
+        ],
+        name: "userchoice"
+    }]).then(function(response){
+        switch (response.userchoice){
+            case "View All Employees":
+            viewAll();
+            break;
+
+            case "View All Employees By Department":
+            viewByDepartment();
+            break;
+
+            case "View All Employees By Manager":
+            viewByManager();
+            break;
+
+            case "Add Employee":
+            addEmployee();
+            break;
+
+            case "Remove Employee":
+            removeEmployee();
+            break;
+
+            case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+
+            case "Update Employee Manager":
+            updateEmployeeManager();
+            break;
+
+            case "Done!":
+            console.log("Thank you for using the system! Good-Bye!");
+            break;
+        }
+    });
+}
