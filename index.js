0352e65f4543b2e4a8dbd0fca605bca9783d9808
Oscar Medina <ogmedina04@gmail.com>
2021-01-16 16:11:36 -0600

    added more database information and two new functions to index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "#!Wm#!cKvsq6!bKSnEEh",
     database: "employee_trackerdb"
 })
 
@@ -77,8 +77,53 @@ function promptUser() {
             break;
 
             case "Done!":
-            console.log("Thank you for using the system! Good-Bye!");
+            missionComplete();            
             break;
         }
     });
+}
+
+function viewAll() {
+    let query = "SELECT employee.first_name, employee.last_name, role.title, departments.department, role.salary, employee.manager_id";
+    query += " FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN departments ON (role.department_id = departments.id)";
+    query += " ORDER BY employee.last_name;";
+    connection.query(query, function (err, res){
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    })
+};
+
+function viewByDepartment(){
+    let query = "SELECT departments.department, employee.first_name, employee.last_name";
+    query += " FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN departments ON (role.department_id = departments.id)";
+    query += " ORDER BY departments.department;";
+    connection.query(query, function (err, res){
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    })
+};
+
+function addEmployee(){
+    inquirer.prompt
+}
+
+
+function missionComplete() {
+    CFonts.say('Goodbye!', {
+        font: 'block',              
+        align: 'left',           
+        colors: ['system'], 
+        background: 'transparent',
+        letterSpacing: 1,         
+        lineHeight: 1,            
+        space: true,              
+        maxLength: '0',           
+        gradient: false,           
+        independentGradient: false, 
+        transitionGradient: false,  
+        env: 'node'     
+        });
+    connection.end();    
 }
--- seed.sql
+++ seed.sql
@@ -26,3 +26,71 @@ CREATE TABLE employee (
     manager_id INT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO department(name)
+VALUES ("Sales");
+
+INSERT INTO department(name)
+VALUES ("Engineering");
+
+INSERT INTO department(name)
+VALUES ("Finance");
+
+INSERT INTO department(name)
+VALUES ("Legal");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Chan", 2, 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Ashley", "Rodriguez", 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Tupik", 4, 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Malia", "Brown", 5);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Sarah", "Lourd", 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Allen", 7, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Christian", "Eckenrode", 3, 2);
+
+
+SELECT employee.first_name, employee.last_name, role.title, departments.department, role.salary, employee.manager_id
+FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN departments ON (role.department_id = departments.id)
+ORDER BY employee.last_name;
+
+SELECT departments.department, employee.first_name, employee.last_name
+FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN departments ON (role.department_id = departments.id)
+ORDER BY departments.department;
+
